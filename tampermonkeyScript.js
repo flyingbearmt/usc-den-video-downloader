9b7e226b97dffef4f630afb4ebf615c07731bcd5
flyingbearmt <tongmiao@usc.edu>
2020-03-23 15:02:37 -0700

    fix some small bugs

--- tampermonkeyScript.js
+++ tampermonkeyScript.js
@@ -11,24 +11,28 @@
 // @require      https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
 // ==/UserScript==
 
-(function () {
+(function() {
     'use strict';
 
     var buttonGroup = document.getElementsByClassName("DENVideo");
     let sourceUrl = buttonGroup[1].href;
+    if(sourceUrl == "" || sourceUrl == null){
+        alert("please try again!");
+    }
     //setTimeout(a, 3000);
     //console.log(file);
-    let fileName = "";
+    let fileName= "";
     var httpRequest = new XMLHttpRequest();//第一步：创建需要的对象
-    let para = '?downloadurl=' + encodeURIComponent(sourceUrl) + "&filename=" + fileName;
-    let url = 'http://localhost:8081/download_usc_den_video' + para;
-    httpRequest.open('GET', url, true); //第二步：打开连接
-    httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");//设置请求头 注：post方式必须设置请求头（在建立连接后设置请求头）
+    let para = '?downloadurl='+encodeURIComponent(sourceUrl) +"&filename="+fileName;
+    let url ='http://localhost:8081/download_usc_den_video'+ para;
+    httpRequest.open('GET',url, true); //第二步：打开连接
+    httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");//设置请求头 注：post方式必须设置请求头（在建立连接后设置请求头）
     httpRequest.send();//发送请求 将情头写在体send中
     console.log("usc_den_downloading");
+    alert("start to downloading!");
     /**
-     * 获取数据后的处理程序
-     */
+ * 获取数据后的处理程序
+ */
     httpRequest.onreadystatechange = function () {//请求后的回调接口，可将请求成功后要执行的程序写在其中
         if (httpRequest.readyState == 4 && httpRequest.status == 200) {//验证请求是否发送成功
             var json = httpRequest.responseText;//获取到服务端返回的数据
@@ -37,10 +41,11 @@
         }
     };
 
-    function a() {
+    function a(){
         console.log("i am ppp");
         var docu = document.getElementsByClassName("d2l-page-title")[0].innerText
         //console.log(docu);
+        
     }
 
 })();
